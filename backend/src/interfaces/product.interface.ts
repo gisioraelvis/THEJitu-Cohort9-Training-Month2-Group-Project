45d7242f14b27b4e518266c5780c9b4c5d58ece4
gisioraelvis <gisioraelvis20@gmail.com>
2023-02-26 17:13:04 +0300

    return catories and reviews as array of objects

--- backend/src/interfaces/product.interface.ts
+++ backend/src/interfaces/product.interface.ts
@@ -35,3 +35,36 @@ export interface Product {
   createdAt?: string;
   updatedAt?: string;
 }
+
+export interface IProduct {
+  id: number;
+  userId: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  countInStock: number;
+  brandName: string;
+  categoryId: number;
+  categoryName: string;
+  reviewId: number;
+  rating: number;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IProductObject {
+  id: number;
+  userId: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  countInStock: number;
+  brandName: string;
+  categories: { categoryId: number; categoryName: string }[];
+  reviews: { reviewId: number; rating: number; comment: string }[];
+  createdAt: string;
+  updatedAt: string;
+}

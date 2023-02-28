f5e148fe763bc6eb028ec6678ba53f66dee28a3f
gisioraelvis <gisioraelvis20@gmail.com>
2023-02-28 15:57:39 +0300

    code cleanup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AdminDashboardModule } from './admin-dashboard/admin-dashboard.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerDashboardModule } from './customer-dashboard/customer-dashboard.module';
 import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
@@ -14,8 +12,6 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CustomerDashboardModule,
-    AdminDashboardModule,
     FooterComponent,
     HttpClientModule,
     // ReactiveFormsModule

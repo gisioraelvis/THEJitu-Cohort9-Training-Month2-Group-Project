9786db4bb7d41a6a7d0151b4ff72e6351e93143e
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-28 19:05:13 +0300

    updated code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { CarouselModule } from '@coreui/angular';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +14,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
     AppRoutingModule,
     FooterComponent,
     HttpClientModule,
+    CarouselModule,
     // ReactiveFormsModule
   ],
   providers: [],
--- frontend/src/app/shopping/components/shop/carousel/carousel.component.ts
+++ frontend/src/app/shopping/components/shop/carousel/carousel.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 
-@Component({
-  standalone: true,
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css'],
-})
-export class CarouselComponent {}
+// @Component({
+//   standalone: true,
+//   selector: 'app-carousel',
+//   templateUrl: './carousel.component.html',
+//   styleUrls: ['./carousel.component.css'],
+// })
+// export class CarouselComponent {}
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CarouselModule } from '@coreui/angular';
+
+
+  @Component({
+    standalone: true,
+    selector: 'app-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.css'],
+    imports: [
+      CarouselModule,
+      RouterModule
+    ],
+  })
+
+export class  CarouselComponent implements OnInit {
+
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: 'assets/images/ps5.jpg',
+      title: 'First slide',
+      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+    };
+    this.slides[1] = {
+      id: 1,
+      src: './assets/img/react.jpg',
+      title: 'Second slide',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }
+    this.slides[2] = {
+      id: 2,
+      src: './assets/img/vue.jpg',
+      title: 'Third slide',
+      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+    }
+  }
+}

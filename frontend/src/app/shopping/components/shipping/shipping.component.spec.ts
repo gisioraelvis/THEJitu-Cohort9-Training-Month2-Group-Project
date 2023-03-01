85a31bbe73a4845488270e9f96fb2c6d3d54951c
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-03-01 06:56:29 +0300

    updated code

---
+++ frontend/src/app/shopping/components/shipping/shipping.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShippingComponent } from './shipping.component';
+
+describe('ShippingComponent', () => {
+  let component: ShippingComponent;
+  let fixture: ComponentFixture<ShippingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ ShippingComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShippingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

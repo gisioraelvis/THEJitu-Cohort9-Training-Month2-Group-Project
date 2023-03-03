10a8940f158a2f049deede7cbb1647b8017084a1
gisioraelvis <gisioraelvis20@gmail.com>
2023-03-03 10:44:31 +0300

    admin dashboard

--- frontend/src/app/admin-dashboard/Services/ProductService/orders.service.ts
+++ frontend/src/app/admin-dashboard/Services/ProductService/orders.service.ts
@@ -1,31 +1,38 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, catchError, throwError } from 'rxjs';
+import { API_URL } from 'src/app/constants';
+import { HttpErrorPopupService } from 'src/app/shared/http-error-popup/http-error-popup.service';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
 import { Order } from '../../Interfaces';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrdersService {
+  // private OrdesApi = 'https://angul-a3143-default-rtdb.firebaseio.com/Orders.json';
 
-  // private OrdesApi = 'https://angul-a3143-default-rtdb.firebaseio.com/Orders.json'; 
-   
   // constructor(private http: HttpClient) { }
-  
+
   // getOrders(): Observable<any> {
   //   return this.http.get<any>(this.OrdesApi);
   // }
 
+  public orders: Order[] = [];
 
-
-
-
-
-
-  public orders:Order[]=[]
-
-  constructor(private http:HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private localStorage: LocalStorageService,
+    private httpErrorPopupService: HttpErrorPopupService,
+    private authService: AuthService
+  ) {}
 
   // addOrder(order:Order):void{
   //   // this.products.push(product)
@@ -34,48 +41,52 @@ export class OrdersService {
   //       console.log(response);
   //     })
   //     }
+  token = this.localStorage.getToken() as string;
 
-  getOrders():Observable<Order[]>{
+  getOrders(): Observable<Order[]> {
     // return this.products
-    return this.http.get<Order[]>('https://angul-a3143-default-rtdb.firebaseio.com/Orders.json')
+    return this.http
+      .get<Order[]>(`${API_URL}/orders`, {
+        headers: { Authorization: `Bearer ${this.token}` },
+      })
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 401 || error.status === 403) {
+            this.router.navigate(['/login']);
+          }
+          this.httpErrorPopupService.showError(
+            error.status,
+            error.error.message
+          );
+          return throwError(error);
+        })
+      );
   }
 
-  getOneOrder(id:string):Observable<Order> {
-    return this.http.get<Order>(`https://angul-a3143-default-rtdb.firebaseio.com/Orders/${id}.json`)
+  getOneOrder(id: string): Observable<Order> {
+    return this.http.get<Order>(
+      `https://angul-a3143-default-rtdb.firebaseio.com/Orders/${id}.json`
+    );
   }
 
-  updateOrder(id:string, order:Order){
-  
-    this.http.put(`https://angul-a3143-default-rtdb.firebaseio.com/Orders/${id}.json`,order).subscribe(response=>{
-      console.log(response);
-      
-    })
+  updateOrder(id: string, order: Order) {
+    this.http
+      .put(
+        `https://angul-a3143-default-rtdb.firebaseio.com/Orders/${id}.json`,
+        order
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
- 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-  deleteOrders(id:string){
-    this.http.delete(`https://angul-a3143-default-rtdb.firebaseio.com/Orders/${id}.json`).subscribe(response=>{
-      console.log(response);
-    })
+  deleteOrders(id: string) {
+    this.http
+      .delete(
+        `https://angul-a3143-default-rtdb.firebaseio.com/Orders/${id}.json`
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }
